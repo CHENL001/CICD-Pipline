1cb4f4dc6c9320363acfa388a2c08fb248429160
chenl001 <chetan.talwekar@gmail.com>
2023-07-02 14:53:27 +0800

    Initial commit

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
 provider "github" {
-  token = "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"
+  token = env("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
+")
 }
-
-resource "github_repository" "example" {
-   name = "CICD Pipline"
-   description = "project automation using terraform + github + circle ci + aws"
-
-}
-
